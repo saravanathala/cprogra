2adc15fcb4473371b7b77dc2dd9a9725e850dae7
saravanathala <35689043+saravanathala@users.noreply.github.com>
2018-01-31 16:24:41 +0530

    poistive or negative

---
+++ program.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+int main ()
+{
+cilrscr();
+int a;
+if(a>0);
+printf("poistive");
+else(a<0);
+printf("negative");
+else
+printf("zero");
+return 0;
+getch()
+}
