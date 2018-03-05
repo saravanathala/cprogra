7b4ffc49170df9e01c6f3129857152318bad3617
saravanathala <35689043+saravanathala@users.noreply.github.com>
2018-03-05 15:49:57 +0530

    saravana52(3).c

---
+++ saravana52(3).c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+int(int,int)
+{
+int main(void)
+{
+	int a,b;
+	int result;
+	scanf("%d%d",&a,&b);
+	result=a+b;
+	printf("%d",result);
+  getch();
+	return 0;
+}
