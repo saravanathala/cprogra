64fe17221add635f0b47ec04da16f4b4f4d4df42
saravanathala <35689043+saravanathala@users.noreply.github.com>
2018-03-09 13:19:19 +0530

    saravana9 4(3).c

---
+++ saravana9 4(3).c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+char s[50];
+int n,i;
+printf("enter the string\n");
+scanf("%s",&s[i]);
+printf("enter the limit\n");
+  sacnf("%d",&n);
+  for(i=0;i<n;i++)
+  {
+    printf("%c",s[i]);
+    getch();
+    return 0;
+  }
+}
