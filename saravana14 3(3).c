cb7559088d027d8a556e1533d76776e8ffa0546a
saravanathala <35689043+saravanathala@users.noreply.github.com>
2018-03-14 03:27:17 -0700

    saravana14 3(3).c

---
+++ saravana14 3(3).c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#inclue<conio.h>
+int main()
+{
+
+    int a;
+    printf("enter the value\n");
+    scanf("%d",&a);
+    a=a*2;
+    printf("the nearest value is:%d",a);
+    getch();
+    return 0;
+ }
